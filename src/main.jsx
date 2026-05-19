e820b6234fc971459c80daaa0b2a6008f8b46be8
himaloydas90 <himaloydas90@gmail.com>
2026-05-19 13:26:21 +0600

    api problem

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
-import { ApiService } from "./Services/Api.js";
-// import { store } from "./Redux/Store.js";
+import { ApiService } from "./services/Api.js";
+
 
 createRoot(document.getElementById("root")).render(
 
